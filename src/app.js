2fea1b0b3c02d4dd45c7896c486973e6eeb0e2dd
wajidkhan99 <peshwears@gmail.com>
2026-04-18 14:43:45 +0500

    Done ho

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const createError = require("./Helpers/errorHelper")
 const app = express();
 
 
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json());
 connectDB()
 
