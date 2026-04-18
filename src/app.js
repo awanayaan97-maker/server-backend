cb3947109839cab5e60b5c3c3c551e4df7c0cf5f
wajidkhan99 <peshwears@gmail.com>
2026-04-18 15:13:19 +0500

    CORS update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "https://extraordinary-moonbeam-cae328.netlify.app/",
+    origin: "https://extraordinary-moonbeam-cae328.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
